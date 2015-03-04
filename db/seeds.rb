082b53c8f417f0b99f13fc86565faedbd9a99406
Bitmaker Labs <BitmakerLabs@Bitmaker03.local>
2015-03-04 17:23:04 -0500

    Added date range to grab multiple dates.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,33 +5,42 @@ require "pry"
 
 
 base_url = "http://www.blogto.com/events/"
-events_page = Nokogiri::HTML(open(base_url))
 
-events_links = events_page.css(".events-item .event-name a")
-                          .map{|x| "http://www.blogto.com" + x.attr('href') }
+day_base_url = "http://www.blogto.com/events/?date="
 
-puts "Fetching events...".yellow
+date_range = (Date.today..(Date.today + 10)).to_a.map(&:to_s)
 
-events_links.map do |event_url|
-  page = Nokogiri::HTML(open(event_url))
-  begin
-    if Event.where(title: page.css("h1.title").text, date: page.css(".info-eventdate").text.to_date).present?
-      print "|".yellow # alread exists in db
-    else
-      event = Event.create!(
-        title:    page.css("h1.title").text,
-        body:     page.css("#content-body").to_html,
-        date:     page.css(".info-eventdate").text.strip,
-        time:     page.css(".info-eventtime").text.strip,
-        venue:    page.css(".venue-name").text.strip,
-        address:  page.css(".location").text.strip,
-        website:  (page.css(".info-website").attr('href').value unless page.css(".info-website").empty?)
-      )
-      print "|".green
+date_urls = date_range.map{|url| day_base_url + url }
+
+date_urls.each do |date_url|
+  events_page = Nokogiri::HTML(open(date_url))
+
+  events_links = events_page.css(".events-item .event-name a")
+                            .map{|x| "http://www.blogto.com" + x.attr('href') }
+
+  puts "\nFetching #{date_url}..."
+
+  events_links.map do |event_url|
+    page = Nokogiri::HTML(open(event_url))
+    begin
+      if Event.where(title: page.css("h1.title").text, date: page.css(".info-eventdate").text.to_date).present?
+        print "|".yellow # alread exists in db
+      else
+        event = Event.create!(
+          title:    page.css("h1.title").text,
+          body:     page.css("#content-body").to_html,
+          date:     page.css(".info-eventdate").text.strip,
+          time:     page.css(".info-eventtime").text.strip,
+          venue:    page.css(".venue-name").text.strip,
+          address:  page.css(".location").text.strip,
+          website:  (page.css(".info-website").attr('href').value unless page.css(".info-website").empty?)
+        )
+        print "|".green
+      end
+    rescue => e
+      puts "\n" + e.to_s.red
+      puts event_url.to_s.red
+      binding.pry
     end
-  rescue => e
-    puts "\n" + e.to_s.red
-    puts event_url.to_s.red
-    binding.pry
   end
 end
