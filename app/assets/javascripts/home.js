c8d056c049a8d1b5b6098e3fac133ddb84385a8d
Josh Teneycke <josh.teneycke@gmail.com>
2015-03-18 11:53:28 -0400

    Mad styles yo.

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -6,29 +6,18 @@ $(document).ready(function(){
     mapTypeId: google.maps.MapTypeId.ROADMAP
     };
 
-  var infoWindow = new google.maps.InfoWindow();
   var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
 
   $.getJSON('/events.json', function(data) { 
     $.each( data.events, function(i, value) {
 
       var myLatlng = new google.maps.LatLng(value.latitude, value.longitude);
-      var infowindow = new google.maps.InfoWindow({
-        content: value.body
-      }); 
-
       var marker = new google.maps.Marker({
         position: myLatlng,
         map: map,
         title: value.title
       });
 
-//  %h1 {{ title }}
-//  %h2 {{ venue }}
-//  %h3 {{ address }}
-//  %h3 {{ time }}
-//  %p {{ content }}
-//
       var template = $('#template').html();
       var details = {
         title:   value.title,
