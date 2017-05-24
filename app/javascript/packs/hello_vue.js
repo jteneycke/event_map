c2ac5635dc0c6adbab06090a6f45a61f5660e193
Josh Teneycke <josh.teneycke@gmail.com>
2017-05-24 01:24:52 -0400

    Nodes where we want them.

--- app/javascript/packs/hello_vue.js
+++ app/javascript/packs/hello_vue.js
@@ -8,7 +8,7 @@ import Vue from 'vue'
 import App from './app.vue'
 
 document.addEventListener('DOMContentLoaded', () => {
-  document.body.appendChild(document.createElement('hello'))
+  //document.body.appendChild(document.createElement('hello'))
   const app = new Vue(App).$mount('hello')
 
   console.log(app)
